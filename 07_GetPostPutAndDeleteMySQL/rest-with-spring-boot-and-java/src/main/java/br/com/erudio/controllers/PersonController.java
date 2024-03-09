17cf67c2375c0a7e9a32d7f03acd5ab3374e5fe9
Igor Frutuoso <79484560+IgorFrutuoso@users.noreply.github.com>
2024-03-09 17:45:42 -0300

    MySql config, services and contoller

--- 07_GetPostPutAndDeleteMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07_GetPostPutAndDeleteMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,8 +1,11 @@
 package br.com.erudio.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +18,38 @@ import br.com.erudio.services.PersonServices;
 public class PersonController {
 
 	@Autowired
-	private PersonServices services;
+	private PersonServices service;
 
-	@RequestMapping(value = "/{id}",
-			method=RequestMethod.GET,
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.GET, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) throws Exception {
-		return services.findById(id);
+	public Person findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+
+	@RequestMapping(method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
 	}
 }
--- 07_GetPostPutAndDeleteMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonServices.java
+++ 07_GetPostPutAndDeleteMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,29 +1,63 @@
 package br.com.erudio.services;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 
-	private final AtomicLong counter = new AtomicLong();
-
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-	public Person findById(String id) {
+	@Autowired
+	PersonRepository repository;
+	
+	public List<Person> findAll() {
+
+		logger.info("Find all People!");
+
+		return repository.findAll();
+	}
+
+	public Person findById(Long id) {
 
 		logger.info("Find one Person!");
 
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Igor");
-		person.setLastName("Frutuoso");
-		person.setAddress("São Paulo - SP - Vila Pauliceia");
-		person.setGender("Male");
-		return person;
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one Person!");
+		return repository.save(person);
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one Person!");
+		
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deleting one Person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+		
 	}
 }
